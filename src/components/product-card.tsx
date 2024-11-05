563a30434efbba77ca2cbd90d98b1af4d3dd22fb
Shailesh Jadav <jshailesh798@gmail.com>
2024-11-05 22:07:51 +0530

    create home pade and create product-card

---
+++ src/components/product-card.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+type productsProps = {
+  productId: string;
+  photo: string;
+  name: string;
+  price: number;
+  stock: number;
+  handler: () => void;
+};
+const server = "kjsdjgkld";
+
+const ProductCard = ({
+  productId,
+  photo,
+  name,
+  price,
+  stock,
+  handler,
+}: productsProps) => {
+  return (
+    <div className="product-card">
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+      <span>₹{price}</span>
+
+      <div>
+        <button onClick={()=>handler()}>
+          <FaPlus />
+         
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,24 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
-  return (
-    <div>home</div>
-  )
-}
 
-export default Home  
+  const addToCarthandler=()=>{}
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products{" "}
+        <Link to={"/search"} className="findmore">
+          More
+        </Link>
+      </h1>
+      <main>
+        <ProductCard productId="bfjdwf" photo="kjsdjgkld" name="MACKBOOK" price={10000} stock={10} handler={addToCarthandler} photo="https://m.media-amazon.com/images/I/71eXNIDUGjL._SX679_.jpg"/>
+
+      </main>
+    </div>
+  );
+};
+
+export default Home;
