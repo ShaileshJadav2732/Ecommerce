d21e0170fcaee15729baed32ea86c4dc2de21170
Shailesh Jadav <jshailesh798@gmail.com>
2024-11-05 22:46:09 +0530

    add cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,8 +1,27 @@
+const cartItems = [];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.18);
+const shippingCharges = 50;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
   return (
-    <div>cart</div>
-  )
-}
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount - <em>₹{discount}</em>
+        </p>
+        <p>
+          <b>Total : ₹{total}</b>
+        </p>
+      </aside>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
